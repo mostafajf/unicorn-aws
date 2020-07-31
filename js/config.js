5ef93a0bf6929c87ccc92f540dc1ba3bfc605cc0
truth <mustafa@appdomain.com.au>
2020-07-31 15:20:29 +0430

    config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-southeast-1_7CRyfTgQw", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "46mb1q95mhfe6dldi3a8lt9b5f", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-southeast-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
